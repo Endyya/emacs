bd6adf2416be8d3baa50d8b026b46b7de322f443
Endyya <endya@protonmail.com>
2024-12-23 11:10:23 +0100

    Started to configure emacs following this video tutorial https://youtu.be/OaF-N-FuGtc?feature=shared

---
+++ init.el
@@ -0,0 +1,12 @@
+(setq inhibit-startup-message t) ; remove startup message 
+(setq visible-bell t)            ; replace bell allert by screen flash
+
+(menu-bar-mode -1)          ; remove the menu bad
+(tool-bar-mode -1)          ; remove the tool icons
+(scroll-bar-mode -1)        ; remove the scroll bar
+
+(global-display-line-numbers-mode 1) ; display number lines
+
+(load-theme 'deeper-blue t)
+
+
